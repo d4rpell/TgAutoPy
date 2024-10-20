80bdb6758704081c563ce2ae42161fa9263735eb
d4rpell <danizspain82@gmail.com>
2024-10-20 16:57:43 +0200

    Progress bar Implementado | Mayor Rendimiento del Programa | Caracteristicas nuevas

--- index.py
+++ index.py
@@ -1,42 +1,102 @@
-from telethon import TelegramClient, events
+#!/usr/bin/python3
+
+from telethon import TelegramClient
 import configparser # CONFIGS
 import os
-
+from tqdm import tqdm # Barra de Progreso
+import time
 
 config = configparser.ConfigParser()
-config.read('credentials.conf')
+config_leer = config.read('credentials.conf')
 
-api_id = config.get('creds', 'api_id')
-api_hash = config.get('creds', 'api_hash')
-channel_username = config.get('creds', 'channel_username')
+if config_leer:
+    try:
+        api_id = config.get('creds', 'api_id')
+        api_hash = config.get('creds', 'api_hash')
+    except (configparser.NoSectionError, configparser.NoOptionError):
+        print("\n[-] Error: La sección o las opciones no existen en el archivo 'credentials.conf'.\n")
+else:
+    print("\n[-] credentials.conf doesn't exists....\n")
+    time.sleep(1)
+    print("\n If you don't know to how to obtain your api_id & api_hash pls obtain more info here: Log in to your Telegram core: https://my.telegram.org. Go to 'API development tools' and fill out the form. You will get basic addresses as well as the api_id and api_hash parameters required for user authorization.")
+    api_id_request = input("\nWhich your api_id?: ")
+    api_hash_request = input("Which your api_hash?: ")
+    telephone_request = input("Which phone number do you have on you account (Necessary for the SMS Code) (Ex. Country Code + Number -> +34644382914): ")
+    syntax = f"""[creds]
+api_id = {api_id_request}
+api_hash = {api_hash_request}
+telefono = {telephone_request}
+    """
 
-descargas = 'descargado'
+    api_id = api_id_request
+    api_hash = api_hash_request
+
+    try:
+        with open('credentials.conf', 'w') as credenciales:
+            credenciales.write(syntax)
+    except:
+        print("[-] Some error ocurred on the creation of the configuration files.")
+        exit(0)
+
+
+channel_username = input("From which channel do you want to obtain the media? (Ex. elhackerdotnet): ")
+
+descargas = 'downloads'
 
 session_name = str(api_id)
 
 client = TelegramClient(session_name, api_id, api_hash)
 
+limite_descargas = int(input("How many new messages do you want to download? (Ex. 10): "))
+
+async def barra_de_progreso(current, total):
+    """Callback para mostrar la barra de progreso."""
+    bar.update(current - bar.n)
+
+
+async def download_media_with_progress(message):
+    full_path = os.path.join(descargas, channel_username)
+
+    if not os.path.exists(descargas):
+        os.makedirs(descargas)
+    
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
+
+    if message.document:
+        file_name = message.file.name or f"{message.id}"
+        file_ext = os.path.splitext(file_name)[1]  # Obtener la extension del archivo
+        file_path = os.path.join(full_path, f"{message.id}{file_ext}")
+    else:
+        file_path = os.path.join(descargas, f"{message.id}.media")
+
+    # Crear barra de progreso
+    global bar
+    bar = tqdm(total=message.file.size if message.file else 100, unit="B", unit_scale=True)
+
+    saved_path = await client.download_media(message.media, file=file_path, progress_callback=barra_de_progreso)
+
+    bar.close()
+
+    print(f"Archivo descargado en: {saved_path}")
 
 async def main():
 
     await client.start()
-
-    # AUTENTICACION
-    if not await client.is_user_authorized():
-        print("Usuario no autorizado. Necesita iniciar sesión.")
-        await client.send_code_request(config.get('creds', 'telefono'))
-        code = input('Introduce el código que te llegó por SMS: ')
-        await client.sign_in(config.get('creds', 'telefono'), code)
-
-
-    async for message in client.iter_messages(channel_username, limit=2): # limit para los ultimos mensajes
-        if message.media:  # Filtrar por contenido
-            file_path = os.path.join(descargas, f"{message.id}.media")
-            saved_path = await client.download_media(message.media, file=file_path)
-            print(f"Archivo descargado en: {saved_path}")
-            break 
+    async for message in client.iter_messages(channel_username, limit=limite_descargas):
+            if message.media:
+                await download_media_with_progress(message)
 
 
 # Ejecutar el cliente
 with client:
     client.loop.run_until_complete(main())
+
+
+""" EN CASO DE QUERER QUE EL PROGRAMA SE EJECUTE EN TIEMPO REAL.
+while True:
+    async for message in client.iter_messages(channel_username, limit=limite_descargas):
+        if message.date > tate and message.media:
+            await download_media_with_progress(message)
+    await asyncio.sleep(60) # Esperar 60 segundos para ver si hay algo nuevo
+"""
